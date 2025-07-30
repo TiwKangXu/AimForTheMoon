0ede23799ff5e2078028c7e45e7a7fd377e8331c
TiwKangXu <e0960467@u.nus.edu>
2025-07-31 00:10:56 +0800

    update keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from threading import Thread
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Bot is running!"
-
+    if request.method == "POST":
+        return "OK", 200
+    return "Bot is running!", 200
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
     t = Thread(target=run)
